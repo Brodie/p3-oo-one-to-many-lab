04ea93c39341bc0591665e63c2cb932d99a796b4
Brodie Ashcraft <brodiea19@gmail.com>
2023-08-05 14:37:53 -0700

    complete one to many lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,39 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    def get_pet_type(self):
+        return self._pet_type
+
+    def set_pet_type(self, pet_type):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception
+        self._pet_type = pet_type
+
+    pet_type = property(get_pet_type, set_pet_type)
+
 
 class Owner:
-    pass
+    pets = []
+
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        our_pets = [pet for pet in Pet.all if pet.owner == self]
+        return sorted(our_pets, key=lambda pet: pet.name)
